531a0af15cdaf92e18cee0cccb7d61f9e9635915
cac0709 <cac91053@gmail.com>
2018-06-12 14:52:39 +0800

    MazeMain2

--- src/com/java2/oo/MazeMain.java
+++ src/com/java2/oo/MazeMain.java
@@ -3,13 +3,14 @@ package com.java2.oo;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
 /*
  * 有一遊戲場地，長寬由執行時決定，場地中亂數產生陷阱 陷阱數也由當時才決定
  * 
- * 有一個玩家在這場地中，初始PH值為100，走一步消耗1HP， 撞到邊界扣5HP，掉到陷阱扣20HP
+ * 有一個玩家在這場地中，初始HP值為100，走一步消耗1HP， 撞到邊界扣5HP，掉到陷阱扣20HP
  * 
  * 有個檔案(maze.txt)資訊如下： 6,4,5,6,8,2,4,2,2,6,6,8,4,2,2,6
  */
@@ -34,10 +35,76 @@ public class MazeMain {
 			while (set.size() < 5) {
 				String trap = String.valueOf(r.nextInt(24));
 				set.add(trap);
+				
+			}
+			// reduce hp
+			int judgment;
+			line = br.readLine();
+			String steps[] = line.split(",");
+			Scanner scanner = new Scanner(System.in);
+			for (int i = 0; i < steps.length; i++) {
+				if (m.player.HP > 0) {
+					judgment = 0;
+					System.out.println("請輸入上(8)下(2)左(4)右(6)：");
+					switch (steps[i]) {
+					//上
+					case "8":
+						if (m.player.location / 6 == 0) {
+							System.out.println("邊界!");
+							m.player.HP -= 5;
+						} else {
+							m.player.location -= 6;
+							for (int a = 0; a < set.size(); a++) {
+								if (m.trapLocation[a] == m.player.location) {
+									judgment = 1;
+								}
+							}
+							if (judgment == 1) {
+								m.player.HP -= 20;
+							} else {
+								m.player.HP -= 1;
+							}
+						}
+						if (m.player.HP > 0) {
+							System.out.println("目前位置：" + m.player.location);
+							System.out.println("剩餘血量：" + m.player.HP);
+							break;
+						} else {
+							System.out.println("YOU DEAD!");
+							break;
+						}
+						//左
+					case "4":
+						if (m.player.location % 6 == 0) {
+							System.out.println("撞牆!");
+							m.player.HP -= 5;
+						} else {
+							m.player.location -= 1;
+							for (int a = 0; a < set.size(); a++) {
+								if (m.trapLocation[a] == m.player.location) {
+									judgment = 1;
+								}
+							}
+							if (judgment == 1) {
+								m.player.HP -= 20;
+							} else {
+								m.player.HP -= 1;
+							}
+						}
+						if (m.player.HP > 0) {
+							System.out.println("目前位置：" + m.player.location);
+							System.out.println("剩餘血量：" + m.player.HP);
+							break;
+						} else {
+							System.out.println("YOU DEAD!");
+							break;
+						}
+						
 			}
 
 		}
-	}
+	
+			}
 	
 	
 
