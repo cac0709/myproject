1d38222d18310ede7bb27247faf8ec9526941b7c
cac0709 <cac91053@gmail.com>
2018-06-12 15:38:30 +0800

    BINGO表格

--- src/com/java2/oo/BingoMain.java
+++ src/com/java2/oo/BingoMain.java
@@ -1,10 +1,32 @@
 package com.java2.oo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class BingoMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			public BingoMain() {
+			try {
+				//FORM形成
+					FileReader fr = new FileReader("bingo.txt");
+					BufferedReader br = new BufferedReader(fr);
+					String line = br.readLine();
+					int number = Integer.parseInt(line);
+					int table =  (int) Math.sqrt(number);
+					//System.out.println(number);
+					//System.out.println(table);
+					Form form = new Form(table,number);
+					form.print();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
+	
 
 }
